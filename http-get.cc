31fc4c6afd9eece42725373c7efa29c84a9e2585
Steven La <mrstevenla@ucla.edu>
2012-05-11 20:25:47 -0700

    I made shit commit messages

--- http-get.cc
+++ http-get.cc
@@ -3,6 +3,7 @@
 // C++ Libraries
 #include <iostream>
 #include <string>
+#include <fstream>
 
 // C Libraries
 #include <stdio.h>
@@ -26,19 +27,32 @@ using namespace std;
 
 int main (int argc, char *argv[])
 {
+  // Requires 1+1 arg exactly
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: %s <absolute_uri>\n", argv[0]);
+    return 1;
+  }
+
   // Connect to local server
   int server_fd = make_client_connection("localhost", PROXY_SERVER_PORT);
+  if (server_fd < 0)
+  {
+    fprintf(stderr, "Couldn't establish connection to local proxy\n");
+    return 1;
+  }
 
   // Prepare request
   string client_req = "GET ";
   client_req.append(argv[1]);
   client_req.append(" HTTP/1.0\r\n\r\n");
 
-  cout << client_req << endl;
-
   // Send request to server
   if (send(server_fd, client_req.c_str(), client_req.length(), 0) == -1)
+  {
     perror("send");
+    return 1;
+  }
 
   // Receive response
   string server_res;
@@ -46,10 +60,40 @@ int main (int argc, char *argv[])
   if (get_data_from_host(server_fd, server_res) != 0)
   {
     // Couldn't get data
-    exit(2);
+    fprintf(stderr, "Couldn't get data from host\n");
+    return 1;
   }
 
-  cout << server_res << endl;
+  // Find location of "\r\n\r\n", after that the data starts
+  size_t header_end = server_res.find("\r\n\r\n", 0);
+  header_end += 4;
+
+  // Make the filename
+  string fname;
+
+  // Find last occurance of '/', move one forward
+  char *fname_slash = strrchr(argv[1], '/');
+  fname_slash++;
+
+  // If last char is slash (next char is null), name is index
+  if (*(fname_slash) == '\0')
+    fname = "index.html";
+
+  // Else name is last section (not including slash)
+  else
+    fname = fname_slash;
+
+  // Write to file
+  ofstream local_file(fname.c_str(), ios::trunc);
+  if (local_file.is_open())
+  {
+    local_file << (server_res.c_str() + header_end);
+    local_file.close();
+  }
+  else
+  {
+    fprintf(stderr, "Unable to open file\n");
+  }
 
   // Close connection to local server
   close(server_fd);
--- http-proxy.cc
+++ http-proxy.cc
@@ -37,6 +37,7 @@ typedef struct
 {
   int client_fd;
   cache_t *cache;
+  pthread_mutex_t mutex;
 }
 pt_params;
 
@@ -44,91 +45,30 @@ pt_params;
  * @brief Receives message fromt client and sends it to remote, back to client
  * @returns 0 if success, 1 if error
  */
-int client_connected (int client_fd, cache_t *cache)
+int client_connected (int client_fd, cache_t *cache, pthread_mutex_t *mutex)
 {
   // Set up buffer
-  char buf[BUFSIZE];
-  size_t buf_len = 0;
+  string client_buffer;
 
   // Loop until we get "\r\n\r\n"
-  while (memmem(buf, buf_len, "\r\n\r\n", 4) == NULL)
+  while (memmem(client_buffer.c_str(), client_buffer.length(), "\r\n\r\n", 4) == NULL)
   {
-    int num_recv = recv(client_fd, buf + buf_len, sizeof(buf) - buf_len, 0);
-    buf_len += num_recv;
+    char buf[BUFSIZE];
+    if (recv(client_fd, buf, sizeof(buf), 0) < 0)
+    {
+      perror("recv");
+      return -1;
+    }
+    client_buffer.append(buf);
   }
 
   // Parse the request, prepare response
+  HttpRequest client_req;
   try {
-    // This is the received request from the client
-    HttpRequest client_req;
-    client_req.ParseRequest(buf, buf_len);
-
-    // We're not doing persistent stuff
-    client_req.ModifyHeader("Connection", "close");
-
-    // Format the new request
-    size_t remote_length = client_req.GetTotalLength() + 1; // +1 for \0
-    char *remote_req = (char *) malloc(remote_length);
-    client_req.FormatRequest(remote_req);
-
-    // If host not specified in first line, find it in the AddHeaders
-    string remote_host;
-    if (client_req.GetHost().length() == 0)
-      remote_host = client_req.FindHeader("Host");
-    else
-      remote_host = client_req.GetHost();
-
-    // String of full path, for searching/adding to cache
-    string full_path = remote_host + client_req.GetPath();
-
-    // Response "from remote host" to send back to client
-    string remote_res;
-
-    // Check cache for response
-    cache_t::iterator it = cache->find(full_path);
-    if (it != cache->end())
-    {
-      // In cache
-      fprintf(stderr, "in cache\n");
-      remote_res = it->second;
-    }
-    else
-    {
-      // Not in cache, gotta make a request for it
-
-      // Make connection to remote host
-      int remote_fd = make_client_connection(remote_host.c_str(), REMOTE_SERVER_PORT);
-
-      fprintf(stderr, "server: sending request\n");
-
-      // Send new request to remote host
-      if (send(remote_fd, remote_req, remote_length, 0) == -1)
-        perror("send");
-
-      fprintf(stderr, "not in cache\n");
-      if (get_data_from_host(remote_fd, remote_res) != 0)
-      {
-        // Couldn't get data
-        exit(2);
-      }
-
-      // Add to the cache
-      cache->insert(pair<string, string>(full_path, remote_res));
-      close(remote_fd);
-    }
-
-    // By now remote_res has entire response, ship that back wholesale back to the client
-    if (send(client_fd, remote_res.c_str(), remote_res.length(), 0) == -1)
-      perror("send");
-
-    //fprintf(stderr, "%s\n", remote_res.c_str());
-
-    free(remote_req);
+    client_req.ParseRequest(client_buffer.c_str(), client_buffer.length());
   }
-
-  // Parsing error
   catch (ParseException ex) {
-    printf("Exception raised: %s\n", ex.what());
+    fprintf(stderr, "Exception raised: %s\n", ex.what());
 
     // When in doubt, assume HTTP/1.0
     string client_res = "HTTP/1.0 ";
@@ -143,16 +83,93 @@ int client_connected (int client_fd, cache_t *cache)
     // Send the bad stuff!
     if (send(client_fd, client_res.c_str(), client_res.length(), 0) == -1)
       perror("send");
-
-    // Close everything and GTFO
-    close(client_fd);
-    //exit(2);
   }
 
-  // Close everything
-  close(client_fd);
-  //exit(0);
+  // We're not doing persistent stuff
+  client_req.ModifyHeader("Connection", "close");
 
+  // Format the new request
+  size_t remote_length = client_req.GetTotalLength() + 1; // +1 for \0
+  char *remote_req = (char *) malloc(remote_length);
+  client_req.FormatRequest(remote_req);
+
+  // If host not specified in first line, find it in the AddHeaders
+  string remote_host;
+  if (client_req.GetHost().length() == 0)
+    remote_host = client_req.FindHeader("Host");
+  else
+    remote_host = client_req.GetHost();
+
+  // String of full path, for searching/adding to cache
+  string full_path = remote_host + client_req.GetPath();
+
+  // Response "from remote host" to send back to client
+  string remote_res;
+
+  // Check cache for response
+  cache_t::iterator it = cache->find(full_path);
+
+  // In cache
+  if (it != cache->end())
+  {
+    //fprintf(stderr, "in cache\n");
+    remote_res = it->second;
+  }
+
+  // Not in cache, make a req for it
+  else
+  {
+    // Make connection to remote host
+    int remote_fd = make_client_connection(remote_host.c_str(), REMOTE_SERVER_PORT);
+    if (remote_fd < 0 )
+    {
+      fprintf(stderr, "Couldn't make a connection to remote host %s on port %s\n", remote_host.c_str(), REMOTE_SERVER_PORT);
+      free(remote_req);
+      return -1;
+    }
+
+    //fprintf(stderr, "server: sending request\n");
+
+    // Send new request to remote host
+    if (send(remote_fd, remote_req, remote_length, 0) == -1)
+    {
+      perror("send");
+      free(remote_req);
+      close(remote_fd);
+      return -1;
+    }
+
+    //fprintf(stderr, "not in cache\n");
+
+    // Store data from remote host into response string
+    if (get_data_from_host(remote_fd, remote_res) != 0)
+    {
+      free(remote_req);
+      close(remote_fd);
+      return -1;
+    }
+
+    // Add to the cache if Cache-Control is not private
+    // This function must be atomic, as the cache is a shared object
+    if (strstr(remote_res.c_str(), "Cache-Control: private") == NULL)
+    {
+      pthread_mutex_lock(mutex);
+      cache->insert(pair<string, string>(full_path, remote_res));
+      pthread_mutex_unlock(mutex);
+    }
+
+    close(remote_fd);
+  }
+
+  // By now remote_res has entire response, ship that back wholesale back to the client
+  if (send(client_fd, remote_res.c_str(), remote_res.length(), 0) == -1)
+  {
+    perror("send");
+    free(remote_req);
+    return -1;
+  }
+
+  free(remote_req);
   return 0;
 }
 
@@ -163,9 +180,10 @@ void *pt_client_connected (void *params)
 {
   // Cast client_fd into an int*, dereference it, call the right function
   pt_params *p = (pt_params *) params;
-  client_connected(p->client_fd, p->cache);
+  client_connected(p->client_fd, p->cache, &p->mutex);
 
-  // Free the malloced client_fd
+  // Close and free
+  close(p->client_fd);
   free(params);
 
   return NULL;
@@ -173,13 +191,18 @@ void *pt_client_connected (void *params)
 
 int main (int argc, char *argv[])
 {
+  // Create a server
+  int sock_fd = make_server_listener(PROXY_SERVER_PORT);
+  if (sock_fd < 0)
+  {
+    fprintf(stderr, "Couldn't make proxy server listener on port %s\n", PROXY_SERVER_PORT);
+    return 1;
+  }
+
   // Initialize cache
   cache_t cache;
 
-  // Create a server
-  int sock_fd = make_server_listener(PROXY_SERVER_PORT);
-
-  printf("server: waiting for connections...\n");
+  //printf("server: waiting for connections...\n");
 
   // Main accept loop
   for (;;)
@@ -198,12 +221,13 @@ int main (int argc, char *argv[])
 
     // Print out IP address
     inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-    printf("server: got connection from %s\n", s);
+    //printf("server: got connection from %s\n", s);
 
     // Set up pthreads parameters
     pt_params *params = (pt_params *) malloc(sizeof(pt_params));
     params->client_fd = client_fd;
     params->cache = &cache;
+    pthread_mutex_init(&params->mutex, NULL);
 
     // Make threads to deal with logic
     pthread_t thread_id; // We're going to detach, IDGAF this variable can die
